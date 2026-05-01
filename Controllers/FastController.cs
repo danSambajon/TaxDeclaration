ba86c33b8037c151c929c1fa54994da25d6490a6
danSambajon <sambajondanaron@gmail.com>
2026-05-01 13:10:05 +0800

    feat: create Fast Tax Declaration boilerplates

---
+++ Controllers/FastController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TaxDeclaration.Models.ViewModels;
+
+namespace TaxDeclaration.Controllers
+{
+    public class FastController : Controller
+    {
+        // go to data folder(server)
+        // copy the station, fastcv, fastledger, fastcoa, stationx from station(with recpos), taxdeclare, company(with recpos)
+        // interface contains:
+        // date range DONE
+        // fast (companies: mmsi, mobility, syvill)
+        // arnontrade (companies: bienes, mcy, mnv personal)
+        // ibs: disbursement, ibs hauling, ibs sales, ibs purchases (company: filpride only)
+        // options excel or pdf
+
+        public IActionResult Index()
+        {
+            var viewModel = new GenerateTaxDeclarationViewModel
+            {
+                Company = "MMSI",
+                CompanyChoices = new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "MMSI", Value = "MMSI" },
+                    new SelectListItem { Text = "Mobility", Value = "Mobility" },
+                    new SelectListItem { Text = "MNV Personal", Value = "MNV Personal" }
+                }
+            };
+
+            viewModel.SelectedCompany = viewModel.CompanyChoices.FirstOrDefault()!.Value;
+
+            return View(viewModel);
+        }
+    }
+}
