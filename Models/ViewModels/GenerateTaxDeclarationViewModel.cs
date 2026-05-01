057288e7c22839a4fa03bc76662262349321b456
danSambajon <sambajondanaron@gmail.com>
2026-05-01 11:59:43 +0800

    feat: create generateTaxDeclarationVM

---
+++ Models/ViewModels/GenerateTaxDeclarationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaxDeclaration.Models.ViewModels
+{
+    public class GenerateTaxDeclarationViewModel
+    {
+        public DateOnly DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public required string Company { get; set; }
+        public required string FileFormat { get; set; }
+    }
+}
