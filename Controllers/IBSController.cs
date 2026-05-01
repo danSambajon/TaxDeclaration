df445211afae01b9ce5bd7e2a691b0d81a7b6363
danSambajon <sambajondanaron@gmail.com>
2026-05-01 09:51:48 +0800

    feature: creation of tax declaration view and controller

---
+++ Controllers/IBSController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaxDeclaration.Controllers
+{
+    public class IBSController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
