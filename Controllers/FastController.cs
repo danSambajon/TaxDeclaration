20401cba06d80108b28f66b388063f6ac9985ae8
danSambajon <sambajondanaron@gmail.com>
2026-05-01 14:26:06 +0800

    fix: remove note comments from the controllers

--- Controllers/FastController.cs
+++ Controllers/FastController.cs
@@ -6,15 +6,6 @@ namespace TaxDeclaration.Controllers
 {
     public class FastController : Controller
     {
-        // go to data folder(server)
-        // copy the station, fastcv, fastledger, fastcoa, stationx from station(with recpos), taxdeclare, company(with recpos)
-        // interface contains:
-        // date range DONE
-        // fast (companies: mmsi, mobility, syvill)
-        // arnontrade (companies: bienes, mcy, mnv personal)
-        // ibs: disbursement, ibs hauling, ibs sales, ibs purchases (company: filpride only)
-        // options excel or pdf
-
         public IActionResult Index()
         {
             var viewModel = new GenerateTaxDeclarationViewModel
