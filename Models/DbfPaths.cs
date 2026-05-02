23b95faab06684d164449cf6d8d770d8c3099081
danSambajon <sambajondanaron@gmail.com>
2026-05-02 14:15:43 +0800

    feat: add the paths of sources in DbfPaths

--- Models/DbfPaths.cs
+++ Models/DbfPaths.cs
@@ -3,5 +3,16 @@
     public class DbfPaths
     {
         public const string StationPath = @"\\192.168.0.253\System3\TaxDeclare\Data\station.dbf";
+
+        public const string Ewt2307DataPath = @"\\192.168.0.253\System3\BIR_Sys2307\Data\";
+        public const string DcrMobilityDataPath = @"\\192.168.0.253\System3\DCR_Mobility\Data\";
+        public const string DcrMMSIDataPath = @"\\192.168.0.253\System3\DCR_MMSI\data\";
+        public const string DcrSyvDataPath = @"\\192.168.0.253\System3\DCR_Syvill\data\";
+        public const string DcrMainDataPath = @"\\192.168.0.253\System3\DCR_Main\Data\";
+        public const string DcrMCYDataPath = @"\\192.168.0.253\System3\DCR_MCY\DATA\";
+        public const string DcrBienesDataPath = @"\\192.168.0.253\System3\DCR_Bienes\DATA\";
+        public const string DcrMNVPDataPath = @"\\192.168.0.253\System3\DCR_MNVP\DATA\";
+
+        public const string IpAddr = "192.168.0.251";
     }
 }
